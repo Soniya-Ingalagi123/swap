92b454ae39724fffd65c810ed83a726385068f5a
Soniya-Ingalagi123 <soiyaingalagi18@gmail.com>
2025-06-11 03:37:32 +0530

    Save

---
+++ s.java
@@ -0,0 +1,20 @@
+package swap;
+public class s {
+      public static void main(String[] args) {
+            int n1=10;
+            int n2=20;
+            int temp;
+
+            System.out.println("Before swapping:");
+            System.out.println("n1="+n1);
+            System.out.println("n2="+n2);
+
+            temp=n1;
+            n1=n2;
+            n2=n1;
+
+            System.out.println("After swapping:");
+            System.out.println("n1="+n1);
+            System.out.println("n2="+n2);
+      }
+}
